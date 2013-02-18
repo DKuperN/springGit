87172c57c2c64ac803c985b9d7854b71b77ed566
Denis <kuperdn@gmail.com>
2013-02-18 21:38:42 +0300

    Добавлен рандомный выбор выходного сообщения

--- src/main/java/HelloWorldMessageProvider.java
+++ src/main/java/HelloWorldMessageProvider.java
@@ -1,10 +1,23 @@
+import java.util.Random;
+
 /**
  * Autor: Денис
  * Date: 06.02.13
  */
 public class HelloWorldMessageProvider implements MessageProviderI {
+    private String message;
+
+    Random random = new Random();
+    private int r = random.nextInt(2);
+
     @Override
     public String getMessage() {
-        return "Hello World";
+        if (r == 1) {
+            message = "Hello world!";
+        } else {
+            message = "Привет мир!";
+        }
+
+        return message;
     }
 }
