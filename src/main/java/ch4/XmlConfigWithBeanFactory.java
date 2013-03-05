fecb3292d04c35e5f4246433f5ab0cbfce438551
DKuperN <kuperdn@gmail.com>
2013-03-05 21:42:28 +0300

    xmlBeanFactory

---
+++ src/main/java/ch4/XmlConfigWithBeanFactory.java
@@ -0,0 +1,22 @@
+package ch4;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.FileSystemResource;
+
+/**
+ * @author Denis Kupriyanchik
+ *         Date: 25.02.13
+ */
+public class XmlConfigWithBeanFactory {
+    public static void main (String[] arg) {
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+
+        XmlBeanDefinitionReader bdr = new XmlBeanDefinitionReader(factory);
+
+        bdr.loadBeanDefinitions(new FileSystemResource("spring/xmlBeanFactory.xml"));
+        Oracle oracle = factory.getBean("oracle", Oracle.class);
+        System.out.println(oracle.defineMeaningOfLife());
+
+    }
+}
